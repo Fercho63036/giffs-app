71523acd6eaed97eaba37000fc98bdc31edbf3e5
ArielParicagua <ArielFParicaguaV@gmail.com>
2023-05-05 19:37:11 -0400

    Peticiones http a la api que nos exporta imagenes a nuestra pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
@@ -16,7 +16,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     GifsModule,
     SharedModule,
-    CommonModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,23 +1,50 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root'})
-
 /*
   providedIn: 'root' => Hacen que este servicio esté totalmente disponible a lo largo de toda la aplicación y todos los módulos que inyecten este servicio.
 */
 
 export class GifsService {
-  private _tagsHistory: string[] = [];
 
-  constructor(){}
+  private _tagsHistory: string[] = [];
+  private apiKey:       string = 'Gy1GEO3Dz6NBqwdJLj0AwCAWreRXneQx';
+  private serviceUrl:   string = 'http://api.giphy.com/v1/gifs';
+
+  constructor( private http: HttpClient ){ }
 
   get tagsHistory() {
     return [...this._tagsHistory];
   }
 
-  searchTag( tag: string ){
-    this._tagsHistory.unshift( tag );
+  private organizeHistory(tag: string){
+    tag = tag.toLowerCase();
 
-    console.log(this.tagsHistory);
+    if(this._tagsHistory.includes(tag)){
+      this._tagsHistory = this._tagsHistory.filter( (oldTag) => oldTag !== tag )
+    }
+
+    this._tagsHistory.unshift( tag );
+    this._tagsHistory = this.tagsHistory.splice(0, 10);
+  }
+
+  // Buscar los valores del tag que la persona esta indicando
+  searchTag( tag: string ):void {
+    if (tag.length === 0) return;
+    this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('apikey', this.apiKey )
+      .set('limit', 10 )
+      .set('q', tag )
+
+    this.http.get(`${ this.serviceUrl }/search`, { params })
+      .subscribe( resp => {
+
+        console.log(resp);
+
+      });
+
   }
 }
